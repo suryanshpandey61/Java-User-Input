c66eba960aced9e009aee4ebf8b1c76f7711a4c6
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-10-16 00:02:55 +0530

    New Program added

---
+++ Program7.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ class Program7 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Choice: ");
+		int choice=sc.nextInt();
+		switch(choice)
+		{
+			case 1:
+				System.out.println("Your choice is: "+choice);
+				break;
+			case 2:
+				System.out.println("Your choice is: "+choice);
+				break;
+			case 3:
+				System.out.println("Your choice is: "+choice);
+				break;
+			case 4:
+				System.out.println("Your choice is: "+choice);
+				break;
+			case 5:
+				System.out.println("Your choice is: "+choice);
+				break;
+			default:
+				System.out.println("This is default statement");
+		}
+	}
+	
+
+}
